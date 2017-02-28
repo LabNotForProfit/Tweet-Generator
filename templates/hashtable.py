f99306154ebab1d57ad18aa7a7e7ee6449c489de
Alex Aaron Pena <alexaaronpena@gmail.com>
2017-02-28 13:05:11 -0800

    [UPDATE] Added run times for each method of hash table

--- templates/hashtable.py
+++ templates/hashtable.py
@@ -18,10 +18,12 @@ class HashTable(object):
         """Return a string representation of this hash table"""
         return 'HashTable({})'.format(repr(self.items()))
 
+    # O(1)
     def _bucket_index(self, key):
         """Return the bucket index where the given key would be stored"""
         return hash(key) % len(self.buckets)
 
+    # O(n^2)
     def keys(self):
         """Return a list of all keys in this hash table"""
         # Collect all keys in each of the buckets
@@ -31,6 +33,7 @@ class HashTable(object):
                 all_keys.append(key)
         return all_keys
 
+    # O(n)
     def values(self):
         """Return a list of all values in this hash table"""
         # TODO: Collect all values in each of the buckets
@@ -39,6 +42,7 @@ class HashTable(object):
             bucket_values += [val[1] for val in bucket.items()]
         return bucket_values
 
+    # O(n)
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table"""
         # Collect all pairs of key-value entries in each of the buckets
@@ -47,6 +51,7 @@ class HashTable(object):
             all_items.extend(bucket.items())
         return all_items
 
+    # O(n)
     def length(self):
         """Return the length of this hash table by traversing its buckets"""
         # TODO: Count number of key-value entries in each of the buckets
@@ -55,6 +60,7 @@ class HashTable(object):
             count += bucket.length()
         return count
 
+    # O(n)
     def contains(self, key):
         """Return True if this hash table contains the given key, or False"""
         # TODO: Check if the given key exists in a bucket
@@ -65,16 +71,19 @@ class HashTable(object):
             return True
         return False
 
+    # O(n)
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError"""
         # TODO: Check if the given key exists and return its associated value
-        bucket_index = hash(key) % len(self.buckets) # Hashes key and maps it to value in bucket range
-        bucket = self.buckets[bucket_index]
-        searched_item = bucket.find(lambda item: item[0] == key)
+        # Hashes key and maps it to value in bucket range
+        bucket_index = hash(key) % len(self.buckets) # O(1)
+        bucket = self.buckets[bucket_index] # O(1)
+        searched_item = bucket.find(lambda item: item[0] == key) # O(n)
         if searched_item is not None:
             return searched_item[1]
         raise KeyError
 
+    # O(n)
     def set(self, key, value):
         """Insert or update the given key with its associated value"""
         # TODO: Insert or update the given key-value entry into a bucket
@@ -90,6 +99,7 @@ class HashTable(object):
         else:
             bucket.append(new_item) # Set new value
 
+    # O(n)
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError"""
         # TODO: Find the given key and delete its entry if found
@@ -120,7 +130,7 @@ def test_hash_table():
     print(ht)
     ht.set('V', 1)
     print(ht)
-    
+
     # Enable this after implementing delete:
     print('Deleting entries:')
     ht.delete('I')
